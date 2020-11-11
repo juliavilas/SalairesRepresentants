978827208fff463198e9032d6e43451444407367
JuVls1 <julia-vilas@outlook.com>
2020-11-11 22:10:13 +0100

    TP terminé

--- src/main/java/representation/Representant.java
+++ src/main/java/representation/Representant.java
@@ -1,88 +1,110 @@
 package representation;
 
+import java.util.HashMap;
+
 public class Representant {
 
-	private final int numero;
-	private final String nom;
-	private final String prenom;
-	private String adresse;
-	private float salaireFixe;
+    private final int numero;
+    private final String nom;
+    private final String prenom;
+    private String adresse;
+    private float salaireFixe;
+    private float[] CAMensuel= new float[12];
+    private ZoneGeographique secteur;
 
-	public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) {
-		this.numero = numero;
-		this.nom = nom;
-		this.prenom = prenom;
-	}
+    public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) {
+        this.numero = numero;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.secteur = secteur;
+    }
 
-	public int getNumero() {
-		return numero;
-	}
+    public int getNumero() {
+        return numero;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public String getAdresse() {
-		return adresse;
-	}
+    public String getAdresse() {
+        return adresse;
+    }
 
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 
-	public float getSalaireFixe() {
-		return salaireFixe;
-	}
+    public float getSalaireFixe() {
+        return salaireFixe;
+    }
 
-	public void setSalaireFixe(float salaireFixe) {
-		this.salaireFixe = salaireFixe;
-	}
+    public void setSalaireFixe(float salaireFixe) {
+        this.salaireFixe = salaireFixe;
+    }
 
-	public ZoneGeographique getSecteur() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public float[] getCAMensuel() {
+        return CAMensuel;
+    }
 
-	public void setSecteur(ZoneGeographique secteur) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public void setCAMensuel(float[] CAMensuel) {
+        this.CAMensuel = CAMensuel;
+    }
 
-	/**
-	 * Enregistre le CA de ce représentant pour un mois donné. 
-	 * @param mois le numéro du mois (de 0 à 11)
-	 * @param montant le CA réalisé pour ce mois (positif ou nul)
-	 **/
-	public void enregistrerCA(int mois, float montant) {
-		// vérifier les paramètres
-		if (mois < 0 || mois > 11) {
-			throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
-		}
-		if (montant < 0) {
-			throw new IllegalArgumentException("Le montant doit être positif ou null");
-		}
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public ZoneGeographique getSecteur() {
+        return secteur;
+    }
 
-	/**
-	 * Calcule le salaire mensuel de ce répresentant pour un mois donné
-	 * @param mois le numéro du mois (de 0 à 11)
-	 * @param pourcentage le pourcentage (>= 0 ) à appliquer sur le CA réalisé pour ce mois
-	 * @return le salaire pour ce mois, tenant compte du salaire fixe, de l'indemnité repas, et du pourcentage sur CA
-	 */
-	public float salaireMensuel(int mois, float pourcentage) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+    public void setSecteur(ZoneGeographique secteur) {
+        this.secteur = secteur;
+    }
 
-	@Override
-	public String toString() {
-		return "Representant{" + "numero=" + numero + ", nom=" + nom + ", prenom=" + prenom + '}';
-	}
+    /**
+     * Enregistre le CA de ce représentant pour un mois donné.
+     *
+     * @param mois le numéro du mois (de 0 à 11)
+     * @param montant le CA réalisé pour ce mois (positif ou nul)
+     *
+     */
+    public void enregistrerCA(int mois, float montant) {
+        // vérifier les paramètres
+        if (mois < 0 || mois > 11) {
+            throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11");
+        }
+        if (montant < 0) {
+            throw new IllegalArgumentException("Le montant doit être positif ou nul");
+        }
+        CAMensuel[mois]=montant;
+        System.out.println("Le chiffre d'afafires mensuel est de :"+ CAMensuel);
+    }
+
+    /**
+     * Calcule le salaire mensuel de ce répresentant pour un mois donné
+     *
+     * @param mois le numéro du mois (de 0 à 11)
+     * @param pourcentage le pourcentage (>= 0 ) à appliquer sur le CA réalisé
+     * pour ce mois
+     * @return le salaire pour ce mois, tenant compte du salaire fixe, de
+     * l'indemnité repas, et du pourcentage sur CA
+     */
+    public float salaireMensuel(int mois, float pourcentage) {
+        if (mois < 0 || mois > 11) {
+            throw new IllegalArgumentException("Le mois doit être compris entre 0 et 11 (0 = janvier)");
+        }
+        if (pourcentage < 0 || pourcentage > 1) {
+            throw new IllegalArgumentException("Le pourcentage doit être compris en 0 et 1 (1=100%)");
+        }
+        float salaire = salaireFixe + secteur.getIndemniteRepas() + pourcentage * CAMensuel[mois];
+        return salaire;
+    }
+
+    @Override
+    public String toString() {
+        return "Representant{" + "numero=" + numero + ", nom=" + nom + ", prenom=" + prenom + ", secteur=" + secteur + '}';
+    }
 
 }
--- src/test/java/representation/TestRepresentant.java
+++ src/test/java/representation/TestRepresentant.java
@@ -1,87 +1,195 @@
 package representation;
 
+import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestRepresentant {
-	// Quelques constantes
-	private static final float FIXE_BASTIDE = 1000f;
-	private static final float INDEMNITE_OCCITANIE = 200f;
-	
-	private Representant r; // L'objet à tester
-	private ZoneGeographique occitanie;
-	
-	@BeforeEach
-	public void setUp() {
-		// Initialiser les objets utilisés dans les tests
-		occitanie = new ZoneGeographique(1, "Occitanie");
-		occitanie.setIndemniteRepas(INDEMNITE_OCCITANIE);
 
-		r = new Representant(36, "Bastide", "Rémi", occitanie);	
-		r.setSalaireFixe(FIXE_BASTIDE);				
-	}
-	
-	@Test
-	public void testSalaireMensuel() {
-		float CA = 50000f;
-		float POURCENTAGE= 0.1f; // 10% de pourcentage sur CA
-		// On enregistre un CA pour le mois 0 (janvier)
-		r.enregistrerCA(0, CA);
-		
-		// On calcule son salaire pour le mois 0 avec 10% de part sur CA
-		float salaire = r.salaireMensuel(0, POURCENTAGE);
-		
-		// A quel résultat on s'attend ?
-		
-		assertEquals(// Comparaison de "float"
-			// valeur attendue
-			FIXE_BASTIDE + INDEMNITE_OCCITANIE + CA * POURCENTAGE,
-			// Valeur calculée
-			salaire,
-			// Marge d'erreur tolérée
-			0.001,
-			// Message si erreur
-			"Le salaire mensuel est incorrect"
-		); 
-	}
+    // Quelques constantes
+    private static final float FIXE_BASTIDE = 1000f;
+    private static final float INDEMNITE_OCCITANIE = 200f;
 
-	@Test
-	public void testCAParDefaut() {
-		float POURCENTAGE= 0.1f; // 10% de pourcentage sur CA
-		
-		// On n'enregistre aucun CA
-		//r.enregistrerCA(0, 10000f);
-		
-		// On calcule son salaire pour le mois 0 avec 10% de part sur CA
-		float salaire = r.salaireMensuel(0, POURCENTAGE);
-		
-		// A quel résultat on s'attend ?
-		// Le CA du mois doit avoir été initialisé à 0
-		
-		assertEquals(
-			FIXE_BASTIDE + INDEMNITE_OCCITANIE, 
-			salaire, 
-			0.001,
-			"Le CA n'est pas correctement initialisé"
-		);
-	}
+    private Representant r; // L'objet à tester
+    private ZoneGeographique occitanie;
 
-	@Test
-	public void testCANegatifImpossible() {
-		
-		try {
-			// On enregistre un CA négatif, que doit-il se passer ?
-			// On s'attend à recevoir une exception
-			r.enregistrerCA(0, -10000f);
-			// Si on arrive ici, c'est une erreur, le test doit échouer
-			fail("Un CA négatif doit générer une exception"); // Forcer l'échec du test			
-		} catch (IllegalArgumentException e) {
-			// Si on arrive ici, c'est normal, c'est le comportement attendu
-		}
+    @BeforeEach
+    public void setUp() {
+        // Initialiser les objets utilisés dans les tests
+        occitanie = new ZoneGeographique(1, "Occitanie");
+        occitanie.setIndemniteRepas(INDEMNITE_OCCITANIE);
 
-	}
-	
-	
+        r = new Representant(36, "Bastide", "Rémi", occitanie);
+        r.setSalaireFixe(FIXE_BASTIDE);
+    }
+
+    @Test
+    public void testGetNumero(){
+        assertEquals(36,r.getNumero(),"getNumero() renvoie le mauvais numéro");
+    }
+    
+    @Test
+    public void testGetNom(){
+        assertEquals("Bastide",r.getNom(),"getNom() renvoie le mauvais nom");
+    }
+    
+    @Test
+    public void testGetPrenom(){
+        assertEquals("Rémi",r.getPrenom(),"getPrenom() renvoie le mauvais prénom");
+    }
+    
+    @Test
+    public void testSetAdresse() {
+        r.setAdresse("2 rue des champs");
+        assertEquals("2 rue des champs", r.getAdresse(), "setAdresse() modifie mal l'adresse");
+    }
+
+    @Test
+    public void testGetAdresse() {
+        r.setAdresse("2 rue des champs");
+        r.getAdresse();
+        assertEquals("2 rue des champs", r.getAdresse(), "getAdresse() renvoie la mauvaise adresse");
+    }
+
+    @Test
+    public void testSetSalaireFixe() {
+        r.setSalaireFixe(FIXE_BASTIDE);
+        assertEquals(FIXE_BASTIDE, r.getSalaireFixe(), "setSalaireFixe() modifie mal le salaire fixe");
+    }
+
+    @Test
+    public void testGetSalaireFixe() {
+        r.setSalaireFixe(FIXE_BASTIDE);
+        r.getSalaireFixe();
+        assertEquals(FIXE_BASTIDE, r.getSalaireFixe(), "getSalaireFixe() renvoie le mauvais salaire");
+    }
+
+    @Test
+    public void testSetCAMensuel() {
+        float tab[] = {50000, 49000, 46000, 53000, 50100, 48000, 51050, 48500, 56100, 50300, 48000, 47500, 49500};
+        r.setCAMensuel(tab);
+        assertEquals(tab, r.getCAMensuel(), "setCAMensuel() modifie mal le CA mensuel");
+    }
+
+    @Test
+    public void testGetCAMensuel() {
+        float tab[] = {50000, 49000, 46000, 53000, 50100, 48000, 51050, 48500, 56100, 50300, 48000, 47500, 49500};
+        r.setCAMensuel(tab);
+        r.getCAMensuel();
+        assertEquals(tab, r.getCAMensuel(), "getCAMensuel() revoie le mauvais CA mensuel");
+    }
+
+    @Test
+    public void testSetSecteur() {
+        r.setSecteur(occitanie);
+        assertEquals(occitanie, r.getSecteur(), "setSecteur() modifie mal le secteur");
+    }
+
+    @Test
+    public void testGetSecteur() {
+        r.setSecteur(occitanie);
+        r.getSecteur();
+        assertEquals(occitanie, r.getSecteur(), "getSecteur() renvoie le mauvais secteur");
+    }
+
+    @Test
+    public void testSalaireMensuel() {
+        float CA = 50000f;
+        float POURCENTAGE = 0.1f; // 10% de pourcentage sur CA
+        // On enregistre un CA pour le mois 0 (janvier)
+        r.enregistrerCA(0, CA);
+
+        // On calcule son salaire pour le mois 0 avec 10% de part sur CA
+        float salaire = r.salaireMensuel(0, POURCENTAGE);
+
+        // A quel résultat on s'attend ?
+		
+        assertEquals(// Comparaison de "float"
+                // valeur attendue
+                FIXE_BASTIDE + INDEMNITE_OCCITANIE + CA * POURCENTAGE,
+                // Valeur calculée
+                salaire,
+                // Marge d'erreur tolérée
+                0.001,
+                // Message si erreur
+                "Le salaire mensuel est incorrect"
+        );
+    }
+
+    @Test
+    public void testSalaireMensuelMois() {
+        try {
+            r.salaireMensuel(36, (float) 0.1);
+            fail("Un mois non compris entre 0 et 11 doit lever une exception");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void testSalaireMensuelPourcentage() {
+        try {
+            r.salaireMensuel(2, 2);
+            fail("Un pourcentage non compris entre 0 et 1 doit lever une exception");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void testCAParDefaut() {
+        float POURCENTAGE = 0.1f; // 10% de pourcentage sur CA
+
+        // On n'enregistre aucun CA
+        r.enregistrerCA(0, 0);
+
+        // On calcule son salaire pour le mois 0 avec 10% de part sur CA
+        float salaire = r.salaireMensuel(0, POURCENTAGE);
+
+        // A quel résultat on s'attend ?
+        // Le CA du mois doit avoir été initialisé à 0
+		
+        assertEquals(
+                FIXE_BASTIDE + INDEMNITE_OCCITANIE,
+                salaire,
+                0.001,
+                "Le CA n'est pas correctement initialisé"
+        );
+    }
+
+    @Test
+    public void testCANegatifImpossible() {
+
+        try {
+            // On enregistre un CA négatif, que doit-il se passer ?
+            // On s'attend à recevoir une exception
+            r.enregistrerCA(0, -10000f);
+            // Si on arrive ici, c'est une erreur, le test doit échouer
+            fail("Un CA négatif doit générer une exception"); // Forcer l'échec du test			
+        } catch (IllegalArgumentException e) {
+            // Si on arrive ici, c'est normal, c'est le comportement attendu
+        }
+
+    }
+
+    @Test
+    public void testEnregistrerCAMois() {
+        try {
+            r.enregistrerCA(16, 48000f);
+            fail("Un mois non compris entre 0 et 11 doit lever une exception");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void testEnregistrerCA() {
+        r.enregistrerCA(3, 50000f);
+        assertEquals(50000f, r.getCAMensuel()[3], "Le CA enregistré n'est pas le bon");
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Representant{numero=36, nom=Bastide, prenom=Rémi, secteur=ZoneGeographique{numero=1, nom=Occitanie, indemniteRepas=200.0}}", r.toString(), "toString() n'affiche pas le bon message");
+    }
 }
